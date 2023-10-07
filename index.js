d93fb2d0160d856a33c1708b8f086f9820501fec
Manish <manishnandwani5055@gmail.com>
2023-10-07 14:10:13 +0530

    cors changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const isDev = app.settings.env === 'development'
 const URL = isDev ? 'http://localhost:3000' : 'https://sketchbook-omega.vercel.app'
-app.use(cors({origin: URL}))
+app.use(cors())
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
